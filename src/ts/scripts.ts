b29e0b427c546a1b82519fc2bc25d5ccb6297ab7
Roman Opalko <roman.opalko@2sic.com>
2020-05-13 09:32:39 +0200

    wip: hotspot

--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -1,1 +1,12 @@
 require('../scss/_Styles.scss');
+
+const hotspotImages = document.querySelectorAll('.hotspots');
+
+hotspotImages.forEach(el => el.addEventListener('click', event => {
+  var bounds = (event.target as HTMLElement).getBoundingClientRect();
+  var x = (event as MouseEvent).clientX - bounds.left;
+  var y = (event as MouseEvent).clientY - bounds.top;
+  var xPercent = x / bounds.width * 100;
+  var yPercent = y / bounds.height * 100;
+  console.log({xPercent: xPercent, yPercent: yPercent});
+}));
