5cc877c93d71c9f09bf24c7a3056884f7b9422f5
Roman Opalko <roman.opalko@2sic.com>
2020-05-13 11:36:31 +0200

    added 2sxc new hotspot func

--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -1,12 +1,31 @@
+declare let $2sxc: any;
+
 require('../scss/_Styles.scss');
 
 const hotspotImages = document.querySelectorAll('.hotspots');
 
 hotspotImages.forEach(el => el.addEventListener('click', event => {
-  var bounds = (event.target as HTMLElement).getBoundingClientRect();
-  var x = (event as MouseEvent).clientX - bounds.left;
-  var y = (event as MouseEvent).clientY - bounds.top;
-  var xPercent = x / bounds.width * 100;
-  var yPercent = y / bounds.height * 100;
-  console.log({xPercent: xPercent, yPercent: yPercent});
+  const target = (event.target as HTMLElement);
+  const e = (event as MouseEvent);
+  const moduleId = target.parentElement.dataset.moduleId;
+  const entityId = target.parentElement.dataset.entityId
+  const guid = target.parentElement.dataset.guid
+  const bounds = target.getBoundingClientRect();
+  const x = e.clientX - bounds.left;
+  const y = e.clientY - bounds.top;
+  const xPercent = x / bounds.width * 100;
+  const yPercent = y / bounds.height * 100;
+
+  $2sxc(moduleId).manage.run({
+    'action': 'new',
+    'sortOrder': 0,
+    'isPublished': true,
+    'entityId': entityId,
+    'parent': guid,
+    'fields': 'Hotspots',
+    'prefill': {
+      'X': xPercent,
+      'Y': yPercent
+    }
+  });
 }));
