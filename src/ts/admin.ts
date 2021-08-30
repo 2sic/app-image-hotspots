879c42efece697812704e38aedc9b4f6bb959c18
Roman Opalko <roman.opalko@2sic.com>
2021-08-30 08:06:25 +0200

    fixed demo items, fixed missing toolbar

--- src/ts/admin.ts
+++ src/ts/admin.ts
@@ -3,41 +3,38 @@ declare let $2sxc: any;
 /** Loader function for our Hotspot Admin */
 export function activateAdmin() {
   /* Hotspot */
-  if($('.hotspots').hasClass('hotspots-admin')) {
+  $('.hotspots.hotspots-admin').each(function() {
+    $(this).off().on('click', function() {
+      const target = (event.target as HTMLElement);
+  
+      if(!target.classList.contains('hotspot-image')) {
+        return false;
+      }
     
-    $('.hotspots').each(function() {
-      $(this).off().on('click', function() {
-        const target = (event.target as HTMLElement);
+      const e = (event as MouseEvent);
+      const moduleId = target.parentElement.dataset.moduleId;
+      const entityId = target.parentElement.dataset.entityId
+      const guid = target.parentElement.dataset.guid
+      const bounds = target.getBoundingClientRect();
+      const iconOffsetX = target.parentElement.dataset.iconoffsetX;
+      const iconOffsetY = target.parentElement.dataset.iconoffsetY;
+      const x = e.clientX - bounds.left - parseInt(iconOffsetX);
+      const y = e.clientY - bounds.top - parseInt(iconOffsetY);
+      const xPercent = x / bounds.width * 100;
+      const yPercent = y / bounds.height * 100;
     
-        if(!target.classList.contains('hotspot-image')) {
-          return false;
+      $2sxc(moduleId).manage.run({
+        'action': 'new',
+        'sortOrder': 0,
+        'isPublished': true,
+        'entityId': entityId,
+        'parent': guid,
+        'fields': 'Hotspots',
+        'prefill': {
+          'X': Math.round(xPercent * 100) / 100,
+          'Y': Math.round(yPercent * 100) / 100
         }
-      
-        const e = (event as MouseEvent);
-        const moduleId = target.parentElement.dataset.moduleId;
-        const entityId = target.parentElement.dataset.entityId
-        const guid = target.parentElement.dataset.guid
-        const bounds = target.getBoundingClientRect();
-        const iconOffsetX = target.parentElement.dataset.iconoffsetX;
-        const iconOffsetY = target.parentElement.dataset.iconoffsetY;
-        const x = e.clientX - bounds.left - parseInt(iconOffsetX);
-        const y = e.clientY - bounds.top - parseInt(iconOffsetY);
-        const xPercent = x / bounds.width * 100;
-        const yPercent = y / bounds.height * 100;
-      
-        $2sxc(moduleId).manage.run({
-          'action': 'new',
-          'sortOrder': 0,
-          'isPublished': true,
-          'entityId': entityId,
-          'parent': guid,
-          'fields': 'Hotspots',
-          'prefill': {
-            'X': Math.round(xPercent * 100) / 100,
-            'Y': Math.round(yPercent * 100) / 100
-          }
-        });
-      })
-    });
-  }
+      });
+    })
+  });
 }
