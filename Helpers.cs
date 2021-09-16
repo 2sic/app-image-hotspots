ace5076ce828760bc8b5eb52ecd23102505ba636
Maxim Hüsler <maxim.huesler@2sic.com>
2021-09-16 11:45:34 +0200

    minor changes helpers

--- Helpers.cs
+++ Helpers.cs
@@ -5,11 +5,11 @@ public class Helpers: Custom.Hybrid.Code12
   // returns required attributes for editing mode if needed
   public dynamic EditAttr(int moduleId, dynamic data, dynamic settings) {
     if(!Edit.Enabled) return null;
-    return "data-module-id='" + moduleId + "'"
-      + " data-entity-id='" + data.EntityId + "'"
-      + " data-guid='" + data.EntityGuid + "'"
-      + " data-iconoffset-x='" + settings.HotspotSetting.HotspotOffsetX + "'"
-      + " data-iconoffset-y='" + settings.HotspotSetting.HotspotOffsetY + "'"; 
+    return "data-module-id='" + moduleId
+      + "' data-entity-id='" + data.EntityId
+      + "' data-guid='" + data.EntityGuid
+      + "' data-iconoffset-x='" + settings.HotspotSetting.HotspotOffsetX
+      + "' data-iconoffset-y='" + settings.HotspotSetting.HotspotOffsetY + "'"; 
   }
 
   public dynamic MarkerStyles(dynamic hotspot, dynamic settings) {
@@ -19,6 +19,3 @@ public class Helpers: Custom.Hybrid.Code12
       + " height: " + settings.HotspotSetting.HotspotHeight;
   }
 }
-
-
-
