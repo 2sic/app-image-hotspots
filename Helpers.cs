0618f6e69433117a036d0a811de8228fce7e3f58
ijungleboy <daniel.mettler@2sic.com>
2021-09-28 11:41:13 +0200

    minor use razor-blade 3.7

--- Helpers.cs
+++ Helpers.cs
@@ -7,18 +7,22 @@ public class Helpers: Custom.Hybrid.Code12
     if(!Edit.Enabled) return null;
 
     // Must wrap in Tag.Custom so the HTML-Encoding will be correct
-    return "data-module-id='" + moduleId
+    return Tag.RawHtml(
+      "data-module-id='" + moduleId
       + "' data-entity-id='" + data.EntityId
       + "' data-guid='" + data.EntityGuid
       + "' data-iconoffset-x='" + settings.HotspotMarker.HotspotOffsetX
-      + "' data-iconoffset-y='" + settings.HotspotMarker.HotspotOffsetY + "'"; 
+      + "' data-iconoffset-y='" + settings.HotspotMarker.HotspotOffsetY + "'"
+    ); 
   }
 
   // This position the marker on the image and sets the size as needed
   public dynamic MarkerStyles(dynamic hotspot, dynamic settings) {
-    return "top: " + Convert.ForCode(hotspot.Y) + "%;" 
+    return Tag.RawHtml(
+      "top: " + Convert.ForCode(hotspot.Y) + "%;" 
       + " left: " + Convert.ForCode(hotspot.X) + "%;" 
       + " width: " + settings.HotspotMarker.HotspotWidth + ";"
-      + " height: " + settings.HotspotMarker.HotspotHeight;
+      + " height: " + settings.HotspotMarker.HotspotHeight
+    );
   }
 }
