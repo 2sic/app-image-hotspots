25d9a09dd212f21c05a4f62400e68f3b42d174b4
getBlup <daia.robbi@gmail.com>
2022-07-06 09:36:22 +0200

    Fix the add new function

--- src/ts/admin.ts
+++ src/ts/admin.ts
@@ -1,7 +1,4 @@
 import { CommandNames } from '@2sic.com/2sxc-typings';
-// var $2sxc = require('../../node_modules/@2sic.com/2sxc-typings');
-
-// declare let $2sxc: any;
 
 /** Loader function for our Hotspot Admin */
 export function activateAdmin() {
@@ -19,16 +16,18 @@ export function activateAdmin() {
 
         // get all needed information to create new hotspot
         const e = (event as MouseEvent);
-        const moduleId = parseInt(hsElem.parentElement.dataset.moduleId);
-        const entityId = parseInt(hsElem.parentElement.dataset.entityId);
-        const guid = hsElem.parentElement.dataset.guid;
+        // Find the tag with class 'hotspots' because it contains more positioning data
+        const tagWithData = hsElem.closest(".hotspots") as HTMLElement;
+        const moduleId = parseInt(tagWithData.dataset.moduleId);
+        const entityId = parseInt(tagWithData.dataset.entityId);
+        const guid = tagWithData.dataset.guid;
 
         // get image dimensions
         const bounds = hsElem.getBoundingClientRect();
 
         // get hotspot icon offset
-        const iconOffsetX = hsElem.parentElement.dataset.iconoffsetX;
-        const iconOffsetY = hsElem.parentElement.dataset.iconoffsetY;
+        const iconOffsetX = tagWithData.dataset.iconoffsetX;
+        const iconOffsetY = tagWithData.dataset.iconoffsetY;
 
         // get exact position where hotspor marker shoul be added
         const x = e.clientX - bounds.left - parseInt(iconOffsetX);
@@ -37,21 +36,18 @@ export function activateAdmin() {
         const yPercent = y / bounds.height * 100;
         
         // open 2sxc mask with prefilled hotpot coordinates
-        // window.$2sxc(moduleId).manage.run({
-        window.$2sxc(moduleId).cms.run({
+        $2sxc(moduleId).cms.run({
           action: CommandNames.new,
-          //params: {
-            // index: 0,
-            // isPublished: true,
+          params: {
             entityId: entityId,
             parent: guid,
             fields: 'Hotspots',
             prefill: {
               X: Math.round(xPercent * 100) / 100,
-              Y: Math.round(yPercent * 100) / 100
+              Y: Math.round(yPercent * 100) / 100 
             }
-          //}
-        } as any);
+          }
+        });
       });
 
       // added so click listener is only added once
